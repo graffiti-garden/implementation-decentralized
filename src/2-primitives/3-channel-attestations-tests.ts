3b9cb340cef958e1e48656e1c66240b7b48176dd
theia <theia@mit.edu>
2026-01-17 14:01:32 -0500

    better grouping for primitive tests

--- src/2-primitives/3-channel-attestations-tests.ts
+++ src/2-primitives/3-channel-attestations-tests.ts
@@ -10,106 +10,116 @@ import {
 } from "./1-string-encoding";
 
 export function channelAttestationTests() {
-  const allowedAttestationMethods = [CHANNEL_ATTESTATION_METHOD_SHA256_ED25519];
-  const channelAttestations = new ChannelAttestations();
+  describe("Channel Attestation Tests", () => {
+    const allowedAttestationMethods = [
+      CHANNEL_ATTESTATION_METHOD_SHA256_ED25519,
+    ];
+    const channelAttestations = new ChannelAttestations();
 
-  async function randomActor() {
-    const bytes = randomBytes();
-    const str = await new StringEncoder().encode(
-      STRING_ENCODER_METHOD_BASE64URL,
-      bytes,
-    );
-    return `did:web:${str}.com`;
-  }
-  async function randomChannel() {
-    const bytes = randomBytes();
-    return await new StringEncoder().encode(
-      STRING_ENCODER_METHOD_BASE64URL,
-      bytes,
-    );
-  }
+    async function randomActor() {
+      const bytes = randomBytes();
+      const str = await new StringEncoder().encode(
+        STRING_ENCODER_METHOD_BASE64URL,
+        bytes,
+      );
+      return `did:web:${str}.com`;
+    }
+    async function randomChannel() {
+      const bytes = randomBytes();
+      return await new StringEncoder().encode(
+        STRING_ENCODER_METHOD_BASE64URL,
+        bytes,
+      );
+    }
 
-  test("Invalid attestation method", async () => {
-    const actor = await randomActor();
-    await expect(() =>
-      channelAttestations.register("invalid-method", actor),
-    ).rejects.toThrow();
-  });
-
-  for (const method of allowedAttestationMethods) {
-    describe(`Attestation Method: ${method}`, () => {
-      test("get method", async () => {
-        const channel = await randomChannel();
-        const publicId = await channelAttestations.register(method, channel);
-        const methodReturned = await channelAttestations.getMethod(publicId);
-        expect(methodReturned).toEqual(method);
-      });
-
-      test("Idempotent public Ids", async () => {
-        const channel = await randomChannel();
-        const publicId1 = await channelAttestations.register(method, channel);
-        const publicId2 = await channelAttestations.register(method, channel);
-        expect(publicId1).toEqual(publicId2);
-      });
-
-      test("Unique public ids", async () => {
-        const channel1 = await randomChannel();
-        const channel2 = await randomChannel();
-        const publicId1 = await channelAttestations.register(method, channel1);
-        const publicId2 = await channelAttestations.register(method, channel2);
-        expect(publicId1).not.toEqual(publicId2);
-      });
-
-      test("Valid attestation", async () => {
-        const actor = await randomActor();
-        const channel = await randomChannel();
-        const { attestation, channelPublicId } =
-          await channelAttestations.attest(method, actor, channel);
-        const isValid = await channelAttestations.validate(
-          attestation,
-          actor,
-          channelPublicId,
-        );
-        expect(isValid).toBe(true);
-
-        const channelPublicIdSeperate = await channelAttestations.register(
-          method,
-          channel,
-        );
-        expect(channelPublicId).toEqual(channelPublicIdSeperate);
-      });
-
-      test("Invalid attestation with wrong actor", async () => {
-        const actor = await randomActor();
-        const wrongActor = await randomActor();
-        const channel = await randomChannel();
-        const { attestation, channelPublicId } =
-          await channelAttestations.attest(method, actor, channel);
-        const isValid = await channelAttestations.validate(
-          attestation,
-          wrongActor,
-          channelPublicId,
-        );
-        expect(isValid).toBe(false);
-      });
-
-      test("Invalid attestation with wrong channel", async () => {
-        const actor = await randomActor();
-        const channel = await randomChannel();
-        const wrongChannel = await randomChannel();
-        const { attestation, channelPublicId } =
-          await channelAttestations.attest(method, actor, channel);
-        const wrongChannelPublicId = await channelAttestations.register(
-          method,
-          wrongChannel,
-        );
-        const isValid = await channelAttestations.validate(
-          attestation,
-          actor,
-          wrongChannelPublicId,
-        );
-        expect(isValid).toBe(false);
-      });
+    test("Invalid attestation method", async () => {
+      const actor = await randomActor();
+      await expect(() =>
+        channelAttestations.register("invalid-method", actor),
+      ).rejects.toThrow();
     });
-  }
+
+    for (const method of allowedAttestationMethods) {
+      describe(`Attestation Method: ${method}`, () => {
+        test("get method", async () => {
+          const channel = await randomChannel();
+          const publicId = await channelAttestations.register(method, channel);
+          const methodReturned = await channelAttestations.getMethod(publicId);
+          expect(methodReturned).toEqual(method);
+        });
+
+        test("Idempotent public Ids", async () => {
+          const channel = await randomChannel();
+          const publicId1 = await channelAttestations.register(method, channel);
+          const publicId2 = await channelAttestations.register(method, channel);
+          expect(publicId1).toEqual(publicId2);
+        });
+
+        test("Unique public ids", async () => {
+          const channel1 = await randomChannel();
+          const channel2 = await randomChannel();
+          const publicId1 = await channelAttestations.register(
+            method,
+            channel1,
+          );
+          const publicId2 = await channelAttestations.register(
+            method,
+            channel2,
+          );
+          expect(publicId1).not.toEqual(publicId2);
+        });
+
+        test("Valid attestation", async () => {
+          const actor = await randomActor();
+          const channel = await randomChannel();
+          const { attestation, channelPublicId } =
+            await channelAttestations.attest(method, actor, channel);
+          const isValid = await channelAttestations.validate(
+            attestation,
+            actor,
+            channelPublicId,
+          );
+          expect(isValid).toBe(true);
+
+          const channelPublicIdSeperate = await channelAttestations.register(
+            method,
+            channel,
+          );
+          expect(channelPublicId).toEqual(channelPublicIdSeperate);
+        });
+
+        test("Invalid attestation with wrong actor", async () => {
+          const actor = await randomActor();
+          const wrongActor = await randomActor();
+          const channel = await randomChannel();
+          const { attestation, channelPublicId } =
+            await channelAttestations.attest(method, actor, channel);
+          const isValid = await channelAttestations.validate(
+            attestation,
+            wrongActor,
+            channelPublicId,
+          );
+          expect(isValid).toBe(false);
+        });
+
+        test("Invalid attestation with wrong channel", async () => {
+          const actor = await randomActor();
+          const channel = await randomChannel();
+          const wrongChannel = await randomChannel();
+          const { attestation, channelPublicId } =
+            await channelAttestations.attest(method, actor, channel);
+          const wrongChannelPublicId = await channelAttestations.register(
+            method,
+            wrongChannel,
+          );
+          const isValid = await channelAttestations.validate(
+            attestation,
+            actor,
+            wrongChannelPublicId,
+          );
+          expect(isValid).toBe(false);
+        });
+      });
+    }
+  });
 }
